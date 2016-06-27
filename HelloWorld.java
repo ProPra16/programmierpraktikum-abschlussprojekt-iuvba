bcd2e2fa05515f37f9f4ce312aa0f637d2ed224e
padgm <paul.degemann@hhu.de>
2016-06-27 11:58:26 +0200

    print Methode und main getrennt

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,9 @@
 public class HelloWorld {
 
 	public static void main (String[] args) {
+		System.out.print("Hello ");
 		print();
 	}
 	public static void print() {
-		System.out.println("Hello World!");
+		System.out.print("World!");
 }
