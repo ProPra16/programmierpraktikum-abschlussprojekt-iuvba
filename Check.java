57762396b6f8e12a245753637331d8be93cfe8f3
markus2803 <markus.moll@uni-duesseldorf.de>
2016-07-04 13:17:08 +0200

    Update Check.java

--- Check.java
+++ Check.java
@@ -1,21 +1,33 @@
-import package virtual-kata-lib-3f0664dc35c9e2396b1a75b2d6f78ad8620d73df/src/main/java/vk/core;
+package virtual-kata-lib-3f0664dc35c9e2396b1a75b2d6f78ad8620d73df/src/main/java/vk/core;
 import java.util.List;
 import java.util.LinkedList;
-import java.nio.file;
+//import java.nio.file;
 
 public class Check{
 
 	boolean KompilierbarUndEinTestFailed;
+	JavaStringCompiler compiler;
+/*	public static void main(String[] args){
+		checkRED(args[0],args[1],false);
+}*/
 
-	public boolean checkRED(String file, String NameDerDatei, boolean isTest){
+	public static boolean checkRED(String NameDerDatei, String file, boolean isTest){
+			int laenge = NameDerDatei.length();
+			NameDerDatei = NameDerDatei.substring(0,laenge-5);
+			//System.out.print(NameDerDatei);
+			//return false;
+			CompilationUnit compilationUnits = new CompilationUnit(NameDerDatei,file,isTest);
+			compiler = CompilerFactory.getCompiler(compilationUnits);
+			compiler.compilerAndRunTests();
 
 
-		if(...){
 
+		if(!compiler.hasCompileErrors()){
+			KompilierbarUndEinTestFailed = false;
 		}
 		else{ 
 			JavaStringCompiler.compileAndRunTests()
-			if(TestResult.getNumberOfFailedTests() == 1){
+			if(compiler.getNumberOfFailedTests() == 1){
 				return true;
 			}
 			KompilierbarUndEinTestFailed = false;
@@ -24,4 +36,8 @@ public class Check{
 			
 	}
 
+//kopie der Datei erstellen
+
+// compilerfaktory -> getCompilerfkt(CompilationUnit) -> JavaStringKompiler -> FKT zum testen und und und
+}
 }
