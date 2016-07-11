64901b2ccce4b45a9658b0f0028db01a53f09ffd
lomoz <lorcan.mozis@hhu.de>
2016-07-11 13:32:06 +0200

    new gradle project

---
+++ aktuellesProjekt/src/main/java/de/hhu/propra16/Aufgabe.java
@@ -0,0 +1,12 @@
+package de.hhu.propra16;
+
+/**
+ * Created by lorcan on 11.07.16.
+ */
+public class Aufgabe {
+
+    public String Name;
+    public String Beschreibung;
+    public String ClassName;
+    public String Inhalt;
+}
---
+++ aktuellesProjekt/src/main/java/de/hhu/propra16/MenuGUI.java
@@ -0,0 +1,31 @@
+package de.hhu.propra16;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuGUI extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(MenuGUI.class.getResource("LayoutMenu.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		String stylesheet = getClass().getResource("Style.css").toExternalForm();
+		scene.getStylesheets().add(stylesheet);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("TDD Trainer");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+
+}
