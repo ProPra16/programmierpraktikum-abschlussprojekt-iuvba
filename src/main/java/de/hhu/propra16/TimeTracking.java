1865dafff7f51662118770e6561c5a96a383357e
padgm <paul.degemann@hhu.de>
2016-07-21 16:04:55 +0200

    Tracking

---
+++ src/main/java/de/hhu/propra16/TimeTracking.java
@@ -0,0 +1,19 @@
+package de.hhu.propra16;
+
+/**
+ * Created by paul on 21.07.16.
+ */
+
+//private ArrayList<long> timerPerRound = new ArrayList<>();
+
+public class TimeTracking {
+    public void timeTracking (){
+
+/*
+
+        long usedTime = getUserTime();
+        timerPerRound(i);
+*/
+
+    }
+}
