ddd0af6a71a3e6357f5f5901eaafafe34a506001
alonagrimberg <grim.alona@gmail.com>
2016-07-23 20:04:00 +0200

    Add files via upload

---
+++ src/main/java/de/hhu/propra16/CompileTest.java
@@ -0,0 +1,29 @@
+package de.hhu.propra16;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompileTest {
+
+    @Test
+    public void testCompilerAndTestRunner() {
+        String code =	"public class Code {\n" + "   public boolean test() {\n" +
+                "      return true;\n" + "   }\n" + "}";
+
+        String test =	"import static org.junit.Asser.*;\n" + "import org.junit.Test;\n" +
+                "\n" + "public class CodeTest {\n" + "	@Test \n" +
+                "	public void testToTest() {\n" + "		assertEquals(true, Code.test());\n" +
+                "}";
+
+        Compile compiler = new Compile();
+
+        compiler.setCode("Code", code);
+        compiler.setTest("CodeTest", test);
+        compiler.compileCodeAndTestTests();
+
+        assertEquals(false,compiler.compilationErrors());
+        assertEquals(true,compiler.numberOfTestsFailed());
+    }
+
+}
